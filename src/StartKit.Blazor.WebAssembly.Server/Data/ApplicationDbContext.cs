da4af31fde7b43b6fb9b783d0997175805b1b82f
Kseniya Dohil <kseniya.dohil@outlook.com>
2023-07-28 10:14:38 +0200

    Change target framework to .Net 7.0

---
+++ src/StartKit.Blazor.WebAssembly.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using StartKit.Blazor.WebAssembly.Server.Models;
+
+namespace StartKit.Blazor.WebAssembly.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
