96b0b2229928d6d8f4371070dda1e468323be493
Kseniya Dohil <kseniya.dohil@outlook.com>
2023-08-07 12:23:43 +0200

    Integrated PWA updater

--- src/StarterKit.Blazor.WebAssembly.Client/Program.cs
+++ src/StarterKit.Blazor.WebAssembly.Client/Program.cs
@@ -6,6 +6,7 @@ using StarterKit.Blazor.WebAssembly.Client;
 using StarterKit.Blazor.WebAssembly.Client.Services.Contracts;
 using StarterKit.Blazor.WebAssembly.Client.Services.Implementations;
 using StarterKit.Blazor.WebAssembly.Client.States;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -26,5 +27,6 @@ builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
 builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddPWAUpdater();
 
 await builder.Build().RunAsync();
